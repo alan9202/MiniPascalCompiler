3825281b9cd6384bddaafebd079f4dac813a02e9
Surface <Surface@Alan-PC>
2017-09-10 22:55:39 -0500

    Se agrega cuerpo de StatementList

--- src/com/uaem/classes/StatementList.java
+++ src/com/uaem/classes/StatementList.java
@@ -1,6 +1,34 @@
 package com.uaem.classes;
 
+import java.util.Vector;
+
 public class StatementList {
     
+    private Vector statementList;
     
+    public StatementList() {
+        this.statementList = new Vector();
+    }
+    
+    public void addElement(Statement statement) {
+        this.statementList.add(0, statement);
+    }
+    
+    public Statement elementAt(int index) {
+        return (Statement) this.statementList.elementAt(index);
+    }
+
+    /**
+     * @return the statementList
+     */
+    public Vector getStatementList() {
+        return statementList;
+    }
+
+    /**
+     * @param statementList the statementList to set
+     */
+    public void setStatementList(Vector statementList) {
+        this.statementList = statementList;
+    }
 }
