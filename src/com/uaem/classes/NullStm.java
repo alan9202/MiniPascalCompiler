f4a998191dd21834343db8932cd06212e3fa1fd5
Alan Aranda <a.aranda9202@gmail.com>
2017-09-17 21:22:20 -0500

    Se modifica primer sentencia para nulos.

--- src/com/uaem/classes/NullStm.java
+++ src/com/uaem/classes/NullStm.java
@@ -1,8 +1,31 @@
 package com.uaem.classes;
 
 public class NullStm extends Statement{
+    private int line;
+    private int column;
 
+    public NullStm(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+    
     @Override
     public void doAction() {
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
     }
 }
